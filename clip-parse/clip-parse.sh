a4018d288cf5c08a77c1a5f2330c8f8ba1b53ba3
EmilySeville7cfg <EmilySeville7cfg@gmail.com>
2023-02-24 21:33:11 +1000

    Remove leading/trailing spaces in description

--- clip-parse/clip-parse.sh
+++ clip-parse/clip-parse.sh
@@ -175,7 +175,7 @@ parser_output_command_description() {
     declare command_summary="$(sed -nE '/^>/ p' <<<"$page_content")"
     parser_check_command_summary_correctness "$command_summary" || return "$FAIL"
 
-    sed -nE '/^> [^:]+$/ s/^> //p' <<<"$command_summary"
+    sed -nE '/^> [^:]+$/ { s/^> +//; s/ +$//; p; }' <<<"$command_summary"
 }
 
 # parser_check_command_tag_correctness <command-tag>
