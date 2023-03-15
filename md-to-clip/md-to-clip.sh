0dae5c325a6265f99e18d00ec9ecaaa8c3314e5a
EmilySeville7cfg <EmilySeville7cfg@gmail.com>
2023-03-15 20:37:47 +1000

    Update converter: - fix man page invokation

--- md-to-clip/md-to-clip.sh
+++ md-to-clip/md-to-clip.sh
@@ -95,7 +95,7 @@ help() {
   export LESS_TERMCAP_so=$'\e[01;33m'
   export LESS_TERMCAP_ue=$'\e[0m'
   export LESS_TERMCAP_us=$'\e[1;4;31m'
-  man "$PROGRAM_NAME"
+  man "${PROGRAM_NAME%%.sh}"
 }
 
 version() {
